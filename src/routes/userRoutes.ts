8c09941a61f032a99071a2e169146d9981fb0269
wilfredeveloper <pxumtech@gmail.com>
2024-07-24 02:33:00 +0300

    feat: Add endpoint for token rehydration in userRoutes.ts

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,10 +1,11 @@
 import express from 'express'
-import { signup, signin, getAllUsers } from '../controllers/userController'
+import { signup, signin, getAllUsers, rehydrateToken } from '../controllers/userController'
 
 const userRoutes = express.Router()
 
 userRoutes.get('/api/v1/users', getAllUsers)
 userRoutes.post('/api/v1/auth/signup', signup)
 userRoutes.post('/api/v1/auth/signin', signin)
+userRoutes.post('/api/v1/auth/rehydrate-token', rehydrateToken)
 
 export default userRoutes
