e8db433bce5416abfee81f45e264218f20aa4039
wilfredeveloper <pxumtech@gmail.com>
2024-07-24 02:33:11 +0300

    chore: Update jwtUtils.ts with token verification functions

--- src/utils/jwtUtils.ts
+++ src/utils/jwtUtils.ts
@@ -1,11 +1,29 @@
+// import dotenv from 'dotenv';
 import jwt from 'jsonwebtoken';
+require('dotenv').config();
+// dotenv.config();
 
-const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET!;
-const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET!;
+const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET! || "SYRoyXkwqCDYlpR4WHmErHxHkKB0oAJEVPFmmPnmENQ=";
+const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET! || "04FehVpNKREApXeN3v/cRL+DXKClcScbNX9wmzBpE04=";
+
+// console.log("ACCESS_TOKEN_SECRET", ACCESS_TOKEN_SECRET);
+// console.log("REFRESH_TOKEN_SECRET", REFRESH_TOKEN_SECRET);
 
 function verifyToken(token: string) {
     return new Promise((resolve, reject) => {
         jwt.verify(token, ACCESS_TOKEN_SECRET, (err, decoded) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(decoded);
+            }
+        });
+    });
+}
+
+function verifyRefreshToken(token: string) {
+    return new Promise((resolve, reject) => {
+        jwt.verify(token, REFRESH_TOKEN_SECRET, (err, decoded) => {
             if (err) {
                 reject(err);
             } else {
@@ -23,4 +41,4 @@ const generateRefreshToken = (userId: string): string => {
     return jwt.sign({ userId }, REFRESH_TOKEN_SECRET, { expiresIn: '7d' });
 }
 
-export { verifyToken, generateAccessToken, generateRefreshToken };
+export { verifyToken, verifyRefreshToken, generateAccessToken, generateRefreshToken };
