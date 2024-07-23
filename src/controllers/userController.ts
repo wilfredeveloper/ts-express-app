222918db4fe4a8c1706be9fc28a9dacdbceb0306
wilfredeveloper <pxumtech@gmail.com>
2024-07-24 02:32:50 +0300

    chore: Refactor userController.ts for improved readability and error handling

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,10 +1,17 @@
+import dotenv from "dotenv";
 import { Request, Response } from "express";
 import User, { UserRole } from "../models/User";
-import dotenv from "dotenv"
-import { generateAccessToken, generateRefreshToken } from "../utils/jwtUtils";
-import { User as UserType } from "../types/user";
+import {
+  generateAccessToken,
+  generateRefreshToken,
+  verifyRefreshToken,
+} from "../utils/jwtUtils";
 
 dotenv.config();
+
+interface DecodedToken {
+  userId: string;
+}
 
 export const signup = async (req: Request, res: Response) => {
   const {
@@ -18,24 +25,29 @@ export const signup = async (req: Request, res: Response) => {
   try {
     const existingUser = await User.findOne({ email });
     if (existingUser) {
-      return res.status(400).json({ message: "Email already exists" });
+      return res.status(400).json({ error: "Email already exists" });
     }
 
-    const user = new User({ username, email, password, role: role || 'House-hunter' });
+    const user = new User({
+      username,
+      email,
+      password,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      role: role || "House-hunter",
+    });
     await user.save();
 
     const accessToken = generateAccessToken(user._id);
 
-    res
-      .status(201)
-      .json({
-        message: "User created successfully",
-        accessToken: accessToken,
-        user: { id: user._id, username, email, role: user.role },
-      });
+    res.status(201).json({
+      message: "User created successfully",
+      accessToken: accessToken,
+      user: { id: user._id, username, email, role: user.role },
+    });
   } catch (error) {
-    console.log(error)
-    return res.status(500).json({ message: "Server error", error: error });
+    console.log(error);
+    return res.status(500).json({ ServerError: "Server error", error: error });
   }
 };
 
@@ -45,33 +57,34 @@ export const signin = async (req: Request, res: Response) => {
   try {
     const user = await User.findOne({ email });
     if (!user) {
-      return res.status(400).json({ message: "Invalid email or password" });
+      return res.status(400).json({ error: "No user found using that email address" });
     }
 
     const isMatch = await user.comparePassword(password);
     if (!isMatch) {
-      return res.status(400).json({ message: "Invalid email or password" });
+      return res.status(400).json({ error: "Invalid email or password" });
     }
 
     const accessToken = generateAccessToken(user._id);
     const refreshToken = generateRefreshToken(user._id);
 
+    if (!accessToken || !refreshToken) {
+      return res.status(500).json({ error: "Unable to generate Tokens" });
+    }
 
-    res
-      .status(200)
-      .json({
-        message: "Sign in successful",
-        accessToken: accessToken,
-        refreshToken: refreshToken,
-        user: {
-          id: user._id,
-          username: user.username,
-          email: user.email,
-          role: user.role,
-        },
-      });
+    res.status(200).json({
+      message: "Sign in successful",
+      accessToken: accessToken,
+      refreshToken: refreshToken,
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+        role: user.role,
+      },
+    });
   } catch (error) {
-    return res.status(500).json({ message: "Server error", error: error });
+    return res.status(500).json({ ServerError: "Server error", error });
   }
 };
 
@@ -88,6 +101,44 @@ export const getAllUsers = async (req: Request, res: Response) => {
       })),
     });
   } catch (error) {
-    return res.status(500).json({ message: "Server error", error: error });
+    return res.status(500).json({ ServerError: "Server error", error: error });
   }
-}
+};
+
+export const rehydrateToken = async (req: Request, res: Response) => {
+  const { refreshToken } = req.body;
+
+  if (!refreshToken) {
+    return res.status(400).json({ message: "Refresh token is required" });
+  }
+
+  try {
+    const decoded = await verifyRefreshToken(refreshToken) as DecodedToken;
+    const user = await User.findById(decoded.userId);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const accessToken = generateAccessToken(user._id);
+    const newRefreshToken = generateRefreshToken(user._id);
+
+    if (!accessToken || !newRefreshToken) {
+      return res.status(500).json({ message: "Unable to generate Tokens" });
+    }
+
+    res.status(200).json({
+      message: "Token rehydrated",
+      accessToken: accessToken,
+      refreshToken: newRefreshToken,
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+        role: user.role,
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({ ServerError: "Server error", error });
+  }
+};
