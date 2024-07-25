86dbc65b0244036276385ffadcfdb1d93213cb63
wilfredeveloper <pxumtech@gmail.com>
2024-07-25 09:57:32 +0300

    feat: Add RBAC middleware to userRoutes for authorization

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,11 +1,12 @@
 import express from 'express'
-import { signup, signin, getAllUsers, rehydrateToken } from '../controllers/userController'
+import { getAllUsers, rehydrateToken, signin, signup } from '../controllers/userController'
+import rbacMiddleware from '../middleware/rbacMiddleware'
 
 const userRoutes = express.Router()
 
-userRoutes.get('/api/v1/users', getAllUsers)
+userRoutes.get('/api/v1/users', rbacMiddleware(['Super Admin']), getAllUsers)
 userRoutes.post('/api/v1/auth/signup', signup)
 userRoutes.post('/api/v1/auth/signin', signin)
-userRoutes.post('/api/v1/auth/rehydrate-token', rehydrateToken)
+userRoutes.post(`/api/v1/auth/rehydrate-token`, rehydrateToken)
 
 export default userRoutes
